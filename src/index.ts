d68931d34d2dbf89287d1121f1234b9cf253237b
mdbeauche <michael.beauchemin@gmail.com>
2022-02-21 09:10:33 -0800

    typescript express server

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import { config } from "./config";
+
+const app: Application = express();
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: "Response from server" });
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Server is listening on port ${config.PORT}!`);
+});

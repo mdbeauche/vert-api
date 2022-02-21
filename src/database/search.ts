b60a5f7804448526e78e506aa3204964c4a7eb5f
mdbeauche <michael.beauchemin@gmail.com>
2022-02-21 10:22:33 -0800

    /search route working

---
+++ src/database/search.ts
@@ -0,0 +1,28 @@
+const database = [
+  {
+    title: 'test 1',
+    photo: '',
+    shortDescription: 'short 1',
+    description: 'long 1',
+  },
+  {
+    title: 'test 2',
+    photo: '',
+    shortDescription: 'short 2',
+    description: 'long 2',
+  },
+  {
+    title: 'test 2 extra',
+    photo: '',
+    shortDescription: 'short 2 extra',
+    description: 'long 2 extra',
+  },
+];
+
+const search = (title: string) => {
+  console.log('searching ', title);
+
+  return database.filter((itm) => itm.title.includes(title));
+};
+
+export default search;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-import express, {
-  Application, Request, Response, NextFunction,
-} from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
 import config from './config';
+import search from './database/search';
 
 const app: Application = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use('/search', (req: Request, res: Response) => {
+  const { term } = req.body;
+
+  const results = search(term);
+
+  res.status(200).send({ data: results });
+});
+
 // eslint-disable-next-line no-unused-vars
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: 'Response from server' });
+  res.status(200).send({ message: 'Response from server' });
 });
 
 app.listen(config.PORT, () => {

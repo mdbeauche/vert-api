163dd36ec0e12f604a2aae210d7ae5d11b91cec4
mdbeauche <michael.beauchemin@gmail.com>
2022-02-21 17:52:13 -0800

    update test data

--- src/database/search.ts
+++ src/database/search.ts
@@ -1,21 +1,24 @@
 const database = [
   {
     title: 'test 1',
-    photo: '',
+    photo: 'https://via.placeholder.com/150',
     shortDescription: 'short 1',
-    description: 'long 1',
+    description:
+      'long 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
   },
   {
     title: 'test 2',
-    photo: '',
+    photo: 'https://via.placeholder.com/300',
     shortDescription: 'short 2',
-    description: 'long 2',
+    description:
+      'long 2 : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
   },
   {
     title: 'test 2 extra',
-    photo: '',
+    photo: 'https://via.placeholder.com/600',
     shortDescription: 'short 2 extra',
-    description: 'long 2 extra',
+    description:
+      'long 2 extra : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
   },
 ];
 
